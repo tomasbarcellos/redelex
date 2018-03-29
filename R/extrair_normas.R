42437c243868845ed846a6f5e2317650ae382d3c
Tomás Barcellos <tomas.barcellos@agricultura.gov.br>
2018-03-29 17:48:23 -0300

    Inclui função de criar urn e seu teste

--- R/extrair_normas.R
+++ R/extrair_normas.R
@@ -7,12 +7,12 @@
 #'
 #' @examples
 limpar_texto <- function(texto) {
-  texto %>% stringr::str_replace_all("[Nn](o|o-|°-|°|º|º-)? ?(?=\\d)", "Nº ") %>%
+  texto %>% eliminar_quebras() %>%
+    stringr::str_replace_all("[Nn](o|o-|°-|°|º|º-)? ?(?=\\d)", "Nº ") %>%
     stringr::str_replace_all("(?<=\\d)(o-|o|°-|°) ?", "º ") %>%
     stringr::str_replace_all("(?<=\\d)(a-|a) ?", "ª ") %>%
     stringr::str_trim("both") %>%
     # extract(!stringr::str_detect(., "Este documento pode ser verificado no endereço")) %>%
-    eliminar_quebras() %>%
     stringr::str_trim("both") %>%
     magrittr::extract(. != "")
 }
@@ -39,7 +39,7 @@ eliminar_quebras <- function(texto) {
   if (entrou_colapsado) {
     res <- texto # devolver com tamanho 1
   } else {
-    res <- strsplit(texto, "\\n")[[1]] # devolver como vetor
+    res <- stringr::str_split(texto, "\\n") %>% unlist() # devolver como vetor
   }
   res
 }
@@ -62,20 +62,69 @@ extrair_normas <- function(texto) {
   # Aplicar alguns regex no texto
   padroes <- paste0('(?<!-)',
     lista_de_tipos,
-    '.{0,20} nº ?\\d{1,3}(\\.?\\d{3})?(\\/\\d{2,4})?(, ?de \\d{1,2}º? de .+? de \\d{4})?'
+    '.{0,10} nº ?\\d{1,3}(\\.?\\d{3})?(\\/\\d{2,4})?(, ?de \\d{1,2}º? de .+? de \\d{4})?'
   )
 
   # Garantir que não pegue 2 vezes decreto-lei
   # como decreto-lei e como lei
 
   texto_limpo <- texto %>% limpar_texto() %>%
-    stringr::str_to_lower()
+    stringr::str_c(collapse = ' ') %>%
+    stringr::str_to_lower() %>%
+    stringr::str_replace_all('\\(.+?\\)', '')
 
   resp <- purrr::map(
     padroes, ~stringr::str_extract_all(texto_limpo, .x)[[1]]
-    ) %>% unlist()
+  ) %>% unlist()
 
   resp
 
   # E esperamos um vetor com as normas citadas
 }
+
+#' Title
+#'
+#' @param texto
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pegar_nome <- function(texto){
+  texto %>% stringr::str_split('\\n') %>% unlist() %>%
+    `[`(1) %>% #extrair_normas() %>% `[[`(1) %>%
+    stringr::str_to_lower() %>%
+    stringr::str_replace_all('<.+?>', '') %>%
+    stringr::str_replace_all('\\t', '') %>%
+    stringr::str_replace_all('dou \\d{1,2}/\\d{2}/\\d{4}', '') %>%
+    stringr::str_trim('both')
+
+}
+
+#' Title
+#'
+#' @param nome_de_norma
+#'
+#' @return
+#' @export
+#'
+#' @examples
+criar_urn <- function(nome_de_norma) {
+  tipo <- stringr::str_extract(nome_de_norma, '.{1,40}(?= ?nº)') %>%
+    stringr::str_trim()
+  numero <- stringr::str_extract(nome_de_norma, '\\d{1,3}(\\.\\d{3})*') %>%
+    stringr::str_remove_all('\\.')
+  dia <- nome_de_norma %>%
+    stringr::str_extract('\\d{1,2}º? de .{4,9} de \\d{4}') %>%
+    stringr::str_replace('º', '') %>% as.Date(format = '%d de %B de %Y')
+  if (is.na(dia)) {
+    dia <- nome_de_norma %>%
+      stringr::str_extract('\\d{4}')
+    if (is.na(dia)) {
+      dia <- '*'
+    }
+  }
+
+  sprintf('urn:br:%s;%s;%s', tipo, dia, numero)
+}
+
