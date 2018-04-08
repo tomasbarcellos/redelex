bc0b0a6d97414fe1d8fb6bc31573c63433bb3981
tomasbarcellos <tomasbarcellos@gmail.com>
2018-04-07 19:45:25 -0500

    Modificação dos testes para adaptar as mudanças anteriores

--- tests/testthat/teste_criar_urn.R
+++ tests/testthat/teste_criar_urn.R
@@ -5,14 +5,14 @@ titulo2 <- tolower('LEI Nº 11.705/2008')
 titulo3 <- tolower('LEI Nº 11.705')
 
 test_that('Cria URN adequadamente', {
-  expect_identical(criar_urn(titulo1), 'urn:br:lei;2008-06-19;11705')
+  expect_identical(criar_urn(titulo1), as_urn('urn:lex:br:federal:lei:2008-06-19;11705'))
 })
 
 test_that('Lida com datas incompletas', {
-  expect_identical(criar_urn(titulo2), 'urn:br:lei;2008;11705')
+  expect_identical(criar_urn(titulo2), as_urn('urn:lex:br:federal:lei:2008;11705'))
 })
 
 test_that('Lida com datas ausentes', {
-  expect_identical(criar_urn(titulo3), 'urn:br:lei;*;11705')
+  expect_identical(criar_urn(titulo3), as_urn('urn:lex:br:federal:lei:*;11705'))
 })
 
--- tests/testthat/teste_normas_citadas.R
+++ tests/testthat/teste_normas_citadas.R
@@ -1,4 +1,4 @@
-context('Extrair normas')
+context('normas_citadas()')
 
 string_teste <- 'A PRESIDENTA DA REPÚBLICA, no uso da atribuição que lhe confere o art. 84, caput, inciso IV, da Constituição, e tendo em vista o disposto no art. 15 da Decreto-Lei nº 8.666, de 21 de junho de 1993, e no art. 11 da Lei nº 10.520, de 17 de julho de 2002, Lei nº 10, de 17 de julho de 2002,
 O SECRETÁRIO DE DEFESA AGROPECUÁRIA, no uso das atribuições que lhe conferem o artigo 10, do Anexo I, do Decreto Nº 7.127, de 04 de março de 2010, tendo em vista o disposto nos artigos 8º, 37, 69 e 71, do Anexo I, da Instrução Normativa nº 17, de 13 de julho de 2006, do Ministério da Agricultura, Pecuária e Abastecimento, e o que consta do processo nº 21020.002349/2014-21, resolve:
@@ -11,24 +11,26 @@ Art. 2º A suspensão de que trata o art. 1º não se aplica às partidas com li
 Art. 3º Esta Instrução Normativa entra em vigor na data de sua publicação.'
 
 test_that('Regex funciona', {
-  expect_equal(normas_citadas(string_teste),
-               c('lei nº 10.520, de 17 de julho de 2002',
+  # autoinclusao como TRUE pq nao segue padrao e
+  # primeira norma nao e nome
+  expect_equal(normas_citadas(string_teste, FALSE),
+               c('decreto-lei nº 8.666, de 21 de junho de 1993',
+                 'lei nº 10.520, de 17 de julho de 2002',
                  'lei nº 10, de 17 de julho de 2002',
-                 'decreto-lei nº 8.666, de 21 de junho de 1993',
                  'decreto nº 7.127, de 04 de março de 2010',
-                 'decreto nº 24.114, de 12 de abril de 1934',
-                 'instrução normativa nº 17, de 13 de julho de 2006'))
+                 'instrução normativa nº 17, de 13 de julho de 2006',
+                 'decreto nº 24.114, de 12 de abril de 1934'))
 })
 
 path_in01 <- system.file('extdata', 'in01.txt', package = 'redeleg')
 in01 <- readLines(path_in01, encoding = 'UTF-8', warn = FALSE)
+nome_in01 <- "instrução normativa conjunta mp/cgu nº 01, de 10 de maio de 2016"
 
 test_that('Pega todas as normas citadas na IN 01 MP/CGU', {
   expect_equal(normas_citadas(in01),
                c("decreto nº 8.578, de 26 de novembro de 2015",
                  "decreto nº 8.109, de 17 de setembro de 2013"))
   # Nome da norma não aparece como norma citada
-  nome_in01 <- "instrução normativa conjunta mp/cgu nº 01, de 10 de maio de 2016"
   expect_false(nome_in01 %in% normas_citadas(in01))
 })
 
